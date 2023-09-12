2e44e0d1bdb402105bd4ad3a5ce888b2757dd097
Sonu Kumar <sonuhits047@gmail.com>
2023-09-12 22:54:45 +0530

    joke generator

---
+++ backend/Joke_Gen_by_ai/src/main/java/com/masai/JokeGenByAiApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeGenByAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeGenByAiApplication.class, args);
+	}
+
+}
